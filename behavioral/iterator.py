3e17cf8d3a8e07f52f6481b7618518d8ac5267ad
平田　敦 <mummykinoi@gmail.com>
2017-10-02 17:27:46 +0900

    generatorとlambdaについて学習。なんとなく理解。 generatorはyield文を使うことで、関数の実行状態を保持し、next()メソッドが呼ばれるまで次のyield文実行を待機する機能を持つ「関数の一種の型(class)」。 目的、メリットは、generatorを使ってリスト処理等を書くことで、コード上に「長大なリストを引数として渡す」コードを書かなくてよくなること。これによって、無限に長いリストも取り扱えること。 lambdaは関数を式で表現する方法で、名前を持たない関数を記述するところから、lambdaで書かれた関数を無名関数と呼ぶ。 lambdaを利用する目的、メリットは、名前をつけて書くほど利用されない処理について、関数を書かずに済ませられること。これをコードを簡略化するために活用できればメリットである。

--- behavioral/iterator.py
+++ behavioral/iterator.py
@@ -3,18 +3,20 @@
 """
 http://ginstrom.com/scribbles/2007/10/08/design-patterns-python-style/
 Implementation of the iterator pattern with a generator
+ジェネレータを用いたiterator(イテレータ)パターンの例
 """
 
 from __future__ import print_function
 
 
 def count_to(count):
-    """Counts by word numbers, up to a maximum of five"""
+    """数を名前で数え上げます。最大で５(five)までです。
+       この関数の型がgenerator型です。"""
     numbers = ["one", "two", "three", "four", "five"]
     for number in numbers[:count]:
         yield number
 
-# Test the generator
+# generatorを試します。
 count_to_two = lambda: count_to(2)
 count_to_five = lambda: count_to(5)
 
@@ -30,8 +32,17 @@ for number in count_to_five():
 
 print()
 
+# それぞれの関数呼び出しの型が何かを確認しましょう。
+print("count_to_two        type is " + str(type(count_to_two)))
+print("lambda: count_to(2) type is " + str(type(lambda: count_to(2))))
+print("count_to(2)         type is " + str(type(count_to(2))))
+
+
 ### OUTPUT ###
 # Counting to two...
 # one two
 # Counting to five...
 # one two three four five
+# count_to_two        type is <class 'function'>
+# lambda: count_to(2) type is <class 'function'>
+# count_to(2)         type is <class 'generator'>
